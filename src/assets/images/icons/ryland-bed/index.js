47950418915663c579b1aab0c1b9873948c1b43f
actuallyitsnathaniel <nathanielrbowman@gmail.com>
2022-08-09 14:47:46 -0700

    more padding stuff

--- src/assets/images/icons/ryland-bed/index.js
+++ src/assets/images/icons/ryland-bed/index.js
@@ -2,9 +2,9 @@ import rylandBed from "./ryland-bed.svg";
 
 export default function RylandBedIcon() {
   return (
-    <div className="grid h-screen">
+    <div className="grid h-screen lg:pt-5 md:pt-2">
       <img
-        className="max-w-3xl p-10 justify-self-center"
+        className="max-w-3xl justify-self-center p-10"
         alt="ryland-bed-logo"
         aria-label="ryland-bed-logo"
         src={rylandBed}
