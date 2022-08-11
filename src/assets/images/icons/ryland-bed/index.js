96bd5f41978fc044e64570c3578c3a3ed5240709
actuallyitsnathaniel <nathanielrbowman@gmail.com>
2022-08-10 21:02:39 -0700

    fixed bed logo again

--- src/assets/images/icons/ryland-bed/index.js
+++ src/assets/images/icons/ryland-bed/index.js
@@ -2,9 +2,9 @@ import rylandBed from "./ryland-bed.svg";
 
 export default function RylandBedIcon() {
   return (
-    <div className="grid  lg:p-1 ">
+    <div className="grid ">
       <img
-        className="max-w-3xl justify-self-center p-5 sm:p-0 md:p-2 w-screen md:h-screen"
+        className="max-w-2xl justify-self-center sm:p-0 md:p-2 w-screen md:h-screen"
         alt="ryland-bed-logo"
         aria-label="ryland-bed-logo"
         src={rylandBed}
