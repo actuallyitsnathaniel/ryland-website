83a3b0a2028c4fa3fb8422c7aa82db19155d97e8
actuallyitsnathaniel <nathanielrbowman@gmail.com>
2022-08-15 15:42:48 -0700

    fixed onclick for nav items

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -8,18 +8,24 @@ import { ReactComponent as CloseIcon } from "../../assets/images/icons/navbar/cl
 
 function NavBar() {
   const [expanded, setExpanded] = useState(false);
+  var navWrapper = `${expanded ? "block" : "hidden"} md:block`;
+  var navBar =
+    "transition-opacity ease-in-out duration-300 flex flex-col md:flex-row items-center justify-between md:mx-14 my-3 text-3xl whitespace-nowrap";
+  var navItem =
+    "p-1 transition ease-in-out duration-170 hover:scale-110 hover:text-orange-300";
 
   return (
     <nav className="text-white p-2.5 font-semibold">
       <div class="grid grid-cols-2">
-        <a href="/home" className="md:hidden p-3 text-4xl">
+        <a id="person-bed-icon" href="/home" className="md:hidden p-3 text-4xl">
           🛌
         </a>
         <button
-          data-collapse-toggle="navbar-default"
+          data-collapse-toggle="navbar"
+          id="navbar-icon"
           type="button"
           class={`md:hidden p-3 place-self-end`}
-          aria-controls="navbar-default"
+          aria-controls="navbar"
           aria-expanded="false"
           onClick={() => {
             setExpanded(!expanded);
@@ -28,39 +34,41 @@ function NavBar() {
           {expanded ? <CloseIcon /> : <HamburgerIcon />}
         </button>
       </div>
-      <div
-        className={`${expanded ? "block" : "hidden"} md:block`}
-        id="navbar-default"
-      >
-        <ul class="flex flex-col md:flex-row items-center justify-between md:mx-14 my-3 text-3xl whitespace-nowrap">
+      <div className={navWrapper} id="navbar">
+        <ul class={navBar}>
           <Link
-            className="p-1 transition ease-in-out duration-300 hover:scale-110 hover:text-orange-300"
+            className={navItem}
             to="/home"
             aria-current="page"
+            onClick={() => setExpanded(false)}
           >
             Home
           </Link>
           <Link
-            className="p-1 transition ease-in-out duration-300 hover:scale-110 hover:text-orange-300"
+            className={navItem}
             to="/about-us"
+            onClick={() => setExpanded(false)}
           >
             About Us
           </Link>
           <Link
-            className="p-1 transition ease-in-out duration-300 hover:scale-110 hover:text-orange-300"
+            className={navItem}
             to="/contact"
+            onClick={() => setExpanded(false)}
           >
             Contact
           </Link>
           <Link
-            className="p-1 transition ease-in-out duration-300 hover:scale-110 hover:text-orange-300"
+            className={navItem}
             to="/merch"
+            onClick={() => setExpanded(false)}
           >
             Merch
           </Link>
           <Link
-            className="p-1 transition ease-in-out duration-300 hover:scale-110 hover:text-orange-300"
+            className={navItem}
             to="/shows"
+            onClick={() => setExpanded(false)}
           >
             Shows
           </Link>
--- src/components/social-links/index.js
+++ src/components/social-links/index.js
@@ -3,7 +3,7 @@ import GmailLogo from "./gmail-link";
 
 export function SocialLinks() {
   return (
-    <div className="flex lg:fixed md:relative lg:bottom-0 md:bottom-auto lg:left-0 md:left-auto lg:justify-start justify-center md:pt-4 sm:h-full md:h-auto">
+    <div className="flex lg:fixed relative md:bottom-0 bottom-auto md:left-0 left-auto lg:justify-start justify-center md:pt-4 sm:h-full md:h-auto">
       <InstagramLogo />
       <GmailLogo />
     </div>
