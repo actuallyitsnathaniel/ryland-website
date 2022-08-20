def2ce0ca8d0261789e604dd71e6a2c51fa12c5b
nate@lightfeather.io <nate@lightfeather.io>
2022-08-20 14:25:05 -0700

    attempting useState

--- src/pages/about-us.js
+++ src/pages/about-us.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import tom from "../assets/images/the-band/tom.png";
 import ron from "../assets/images/the-band/ron.png";
@@ -7,24 +7,35 @@ import jake from "../assets/images/the-band/jake.png";
 import nate from "../assets/images/the-band/nate.png";
 
 function AboutUs() {
-  const bandMemberBtnStyle =
-    "transition-scale ease-in-out duration-150 hover:scale-110 flex items-center whitespace-nowrap p-3 ";
-
-  // const [expanded, setExpanded] = useState(-1);
+  // using let for now as placeholder
   let expanded = -1;
+
+  // const [expanded, setExpanded] = useState(0);
+
+  const bandMemberBtnStyle =
+    "transition-scale ease-in-out duration-150 hover:scale-110 flex flex-wrap items-center p-3 ";
+
   return (
     <div className="grid place-items-center text-4xl text-white min-h-screen">
       <div className="flex flex-wrap max-w-1/2 justify-center">
         <button
-          // onClick={() => setExpanded(0)}
+          onClick={() => {
+            // setExpanded(0);
+          }}
           className={`${bandMemberBtnStyle}`}
         >
           <img src={tom} alt="tom" className="about-us-profile-pic" />
           <p className="font-bold">Tom</p>
-          <div className={`${expanded === 0 ? "" : ""}`}></div>
+
+          <div className={`text-xs ${expanded === 0 ? "" : "hidden"}`}>
+            lorem ipsum dolor desit requescat en pache lorem ipsum dolor desit
+            requescat en pache lorem ipsum dolor desit requescat en pache lorem
+            ipsum dolor desit requescat en pache lorem ipsum dolor desit
+            requescat en pache
+          </div>
         </button>
 
-        <button className={`${bandMemberBtnStyle}`}>
+        <button onClick={() => {}} className={`${bandMemberBtnStyle}`}>
           <div className="flex items-center whitespace-nowrap p-3">
             <img src={ron} alt="ron" className="about-us-profile-pic" />
             <p className="font-bold">Ron</p>
