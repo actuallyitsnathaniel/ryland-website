7fbd08d89d3f98257c8ea01fca46e730a097c912
nate@lightfeather.io <nate@lightfeather.io>
2022-08-20 00:40:29 -0700

    finished navbar 1.0

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { Link, useLocation, withRouter } from "react-router-dom";
 
 import { ReactComponent as HamburgerIcon } from "../../assets/images/icons/navbar/hamburger-icon.svg";
 import { ReactComponent as CloseIcon } from "../../assets/images/icons/navbar/close-icon.svg";
@@ -9,17 +9,51 @@ import { ReactComponent as CloseIcon } from "../../assets/images/icons/navbar/cl
 function NavBar() {
   const [expanded, setExpanded] = useState(false);
 
+  const [pageTitle, setPageTitle] = useState("");
+
+  let curLoc = useLocation();
+
+  useEffect(() => {
+    const titleMap = [
+      { path: "/", title: "Home" },
+      { path: "/home", title: "Home" },
+      { path: "/about-us", title: "About Us" },
+      { path: "/contact", title: "Contact" },
+      { path: "/music", title: "Music" },
+      { path: "/merch", title: "Merch" },
+      { path: "/shows", title: "Shows" },
+      { path: "/secret", tite: "Secret" },
+    ];
+
+    const curTitle = titleMap.find((item) => item.path === curLoc.pathname);
+    if (curTitle && curTitle.title) {
+      setPageTitle(curTitle.title);
+      document.title = curTitle.title;
+    }
+  }, [curLoc]);
+
   return (
     <nav className="text-white p-2.5 font-semibold">
-      <div class="grid grid-cols-2">
-        <a id="person-bed-icon" href="/home" className="md:hidden p-3 text-4xl">
+      <div class="flex justify-between">
+        <a
+          id="person-bed-icon"
+          href="/home"
+          className="md:hidden p-3.5 text-4xl"
+        >
           🛌
         </a>
+        <div className="md:hidden p-3 justify-center text-5xl whitespace-nowrap">
+          {pageTitle}
+          {/*{[
+            location.pathname.substring(1).charAt(0).toUpperCase(),
+            location.pathname.substring(2),
+          ]}*/}
+        </div>
         <button
           data-collapse-toggle="navbar"
           id="navbar-icon"
           type="button"
-          class={`md:hidden p-3 place-self-end`}
+          class={`md:hidden p-3 justify-end`}
           aria-controls="navbar"
           aria-expanded="false"
           onClick={() => {
@@ -31,13 +65,15 @@ function NavBar() {
       </div>
       <div
         id="nav-wrapper"
-        className={` transition-all h-0 origin-top-right duration-100 md:block
+        className={`transition-all h-0 origin-top duration-100 md:block
         ${expanded ? "scale-100 h-full" : "scale-0"} md:scale-100 md:h-full
         `}
       >
-        <ul id="nav-bar" className={`nav-bar `}>
+        <ul id="nav-bar" className={`nav-bar`}>
           <Link
-            className={"nav-item"}
+            className={`nav-item ${
+              curLoc.pathname === "/home" ? "nav-item-active" : ""
+            }`}
             to="/home"
             aria-current="page"
             onClick={() => setExpanded(false)}
@@ -45,35 +81,45 @@ function NavBar() {
             Home
           </Link>
           <Link
-            className={"nav-item"}
+            className={`nav-item ${
+              curLoc.pathname === "/about-us" ? "nav-item-active" : ""
+            }`}
             to="/about-us"
             onClick={() => setExpanded(false)}
           >
             About Us
           </Link>
           <Link
-            className={"nav-item"}
+            className={`nav-item ${
+              curLoc.pathname === "/contact" ? "nav-item-active" : ""
+            }`}
             to="/contact"
             onClick={() => setExpanded(false)}
           >
             Contact
           </Link>
           <Link
-            className={"nav-item"}
+            className={`nav-item ${
+              curLoc.pathname === "/music" ? "nav-item-active" : ""
+            }`}
             to="/music"
             onClick={() => setExpanded(false)}
           >
             Music
           </Link>
           <Link
-            className={"nav-item"}
+            className={`nav-item ${
+              curLoc.pathname === "/merch" ? "nav-item-active" : ""
+            }`}
             to="/merch"
             onClick={() => setExpanded(false)}
           >
             Merch
           </Link>
           <Link
-            className={"nav-item"}
+            className={`nav-item ${
+              curLoc.pathname === "/shows" ? "nav-item-active" : ""
+            }`}
             to="/shows"
             onClick={() => setExpanded(false)}
           >
@@ -85,4 +131,4 @@ function NavBar() {
   );
 }
 
-export default NavBar;
+export default withRouter(NavBar);
