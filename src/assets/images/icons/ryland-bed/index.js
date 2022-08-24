beaef3faa517dcbdf2b70d91731f817067119b97
nate@lightfeather.io <nate@lightfeather.io>
2022-08-24 10:00:42 -0700

    adjusted bed header for xl screens

--- src/assets/images/icons/ryland-bed/index.js
+++ src/assets/images/icons/ryland-bed/index.js
@@ -4,7 +4,7 @@ export default function RylandBedIcon() {
   return (
     <div className="grid">
       <img
-        className="max-w-2xl justify-self-center md:pb-7 px-12 md:px-0 mt-[35%] md:mt-[130px] mb-[50%] md:mb-[5%] w-screen"
+        className="max-w-2xl justify-self-center md:pb-7 px-12 md:px-0 mt-[35%] md:mt-[130px] xl:mt-[3%] mb-[50%] md:mb-[5%] w-screen xl:h-screen"
         alt="ryland-bed-logo"
         aria-label="ryland-bed-logo"
         src={rylandBed}
