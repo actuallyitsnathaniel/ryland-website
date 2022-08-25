4fd3eedb84c95a585be7911c00269acb90730235
nate@lightfeather.io <nate@lightfeather.io>
2022-08-25 13:48:01 -0700

    modest changes

---
+++ src/pages/about-us/member.js
@@ -0,0 +1,50 @@
+import React, { useState } from "react";
+function BandMember(props) {
+  const [expanded, setExpanded] = useState(-1);
+  const setActiveMember = (i) => {
+    if (i === expanded) {
+      setExpanded(-1);
+    } else {
+      setExpanded(i);
+    }
+  };
+
+  // Tailwind declarations for band members set up an an array
+
+  // 0 = wrapper
+  // 1 = profile-pic
+  // 2 = description text
+
+  const focused = [
+    "fixed z-[1] top-0 grid col-span-1 transition-transform origin-center bg-origin-top duration-150 place-items-center bg-black bg-opacity-70 backdrop-blur-lg w-screen h-screen",
+    "h-[345px] md:h-[400px]",
+    "transition-all duration-75 ease-in-out scale-100 text-sm text-lg text-start mx-10 mb-7",
+  ];
+
+  const unfocused = [
+    "transition-all ease-in-out duration-150 md:hover:scale-110 flex xl:basis-1/3 items-center p-3",
+    "about-us-profile-pic",
+    "scale-0 hidden",
+  ];
+
+  return (
+    <div
+      onClick={() => {
+        setActiveMember(props.i);
+      }}
+      className={`${expanded === props.i ? focused[0] : unfocused[0]}`}
+    >
+      <img
+        src={props.profilePic}
+        alt={props.name}
+        className={`${expanded === props.i ? focused[1] : unfocused[1]}`}
+      />
+      <p className="font-bold self-center p-3">{`${props.name}`}</p>
+      <div className={`${expanded === props.i ? focused[2] : unfocused[2]}`}>
+        {`${props.desc}`}
+      </div>
+    </div>
+  );
+}
+
+export default BandMember;
