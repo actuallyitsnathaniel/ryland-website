14336af110aa7b545cc26548710a86ba13e7d086
nate@lightfeather.io <nate@lightfeather.io>
2023-01-05 11:27:37 -0800

    fixed show date

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -19,9 +19,9 @@ function Home() {
             "https://dice.fm/event/7eyvb-brodyn-john-lowell-anderson-ryland-19th-jan-the-silverlake-lounge-los-angeles-tickets?_branch_match_id=1120861200082607989&_branch_referrer=H4sIAAAAAAAAA8soKSkottLXz8nMy9ZLyUxO1UvL1c81T0kzNzU3NzFJNgEATz3tRyEAAAA%3D"
           }
           venue={"Silverlake Lounge"}
-          date={"January 14th"}
+          date={"January 19th"}
           artists={"Brodyn | John Lowell Anderson | Ryland"}
-          alt={"show-14th"}
+          alt={"show-19th"}
         />
       </div>
 
