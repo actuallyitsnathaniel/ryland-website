878e0467b7c55c104d2608869c5374ca7e10b6e8
nate@lightfeather.io <nate@lightfeather.io>
2023-01-05 11:18:31 -0800

    fixed alignment

--- src/components/show-link/index.js
+++ src/components/show-link/index.js
@@ -1,14 +1,16 @@
 function ShowLink(props) {
   return (
-    <a href={props.link}>
-      <p className="text-3xl text-center font-semibold p-0">{props.venue}</p>
-      <p className="text-center font-semibold p-0">{props.date}</p>
-      <p className="text-center p-0">{props.artists}</p>
-      <img
-        className="transition item-center duration-75 h-72 my-10 hover:-translate-y-1 hover:scale-115"
-        src={props.thumbnail}
-        alt={props.alt}
-      />
+    <a href={props.link} className="text-center">
+      <p className="text-2xl font-semibold">{props.artists}</p>
+      <p className="font-semibold">{props.date}</p>
+      <p className="">{props.venue}</p>
+      <div className="object-center">
+        <img
+          className="mx-auto transition duration-75 h-72 my-10 hover:-translate-y-1 hover:scale-115"
+          src={props.thumbnail}
+          alt={props.alt}
+        />
+      </div>
     </a>
   );
 }
