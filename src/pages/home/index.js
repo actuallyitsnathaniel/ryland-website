0f686f21a7cd623020f4c6d6d4dcb81de02cec3b
nate@lightfeather.io <nate@lightfeather.io>
2023-08-05 10:32:17 -0700

    optimized big svg logo

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -2,7 +2,6 @@ import React from "react";
 
 import RylandBedIcon from "../../assets/images/icons/ryland-bed";
 import { MusicLinks } from ".//../../components/music-embeds";
-import Jan19 from "../../components/show-link/jan19";
 
 function Home() {
   return (
@@ -14,7 +13,6 @@ function Home() {
         <h1 className="p-3 text-[150%] md:text-5xl font-semibold whitespace-nowrap">
           Upcoming Shows
         </h1>
-        <Jan19 />
       </div>
 
       <div>
