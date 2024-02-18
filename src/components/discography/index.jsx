95361630eb1aa54be63d49e8b18a159fa2118753
actuallyitsnathaniel <nathanielrbowman@gmail.com>
2024-02-18 13:00:18 -0800

    edit to file import, removed apostrophe

--- src/components/discography/index.jsx
+++ src/components/discography/index.jsx
@@ -10,7 +10,7 @@ import itinerary from "../../assets/images/discography/Singles_EPs/Itinerary-EP_
 import youshouldknow from "../../assets/images/discography/Singles_EPs/You-Should-Know-EP.jpeg";
 
 // Singles
-import iblfawn from "../../assets/images/discography/Singles_EPs/I'veBeenLooking-Single_600x600bb.jpeg";
+import iblfawn from "../../assets/images/discography/Singles_EPs/IveBeenLooking-Single_600x600bb.jpeg";
 import karma from "../../assets/images/discography/Singles_EPs/Karma-Single_600x600bb.jpeg";
 import stairwell from "../../assets/images/discography/Singles_EPs/Stairwell-Single_600x600bb.jpeg";
 import talking from "../../assets/images/discography/Singles_EPs/Talking-Single_600x600bb.jpeg";
