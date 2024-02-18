7e79015b55b73dcafa18fb2dffe5825c98296eca
actuallyitsnathaniel <nathanielrbowman@gmail.com>
2024-02-18 13:03:37 -0800

    updated export for vite config

--- vite.config.js
+++ vite.config.js
@@ -5,83 +5,85 @@ import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
 import preload from "vite-plugin-preload";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: "",
-  plugins: [
-    react(),
-    svgr(),
-    ViteImageOptimizer({
-      test: /\.(jpe?g|png|gif|tiff|webp|svg|avif)$/i,
-      exclude: undefined,
-      include: undefined,
-      includePublic: true,
-      logStats: true,
-      ansiColors: true,
-      svg: {
-        multipass: true,
-        plugins: [
-          {
-            name: "preset-default",
-            params: {
-              overrides: {
-                cleanupNumericValues: true,
-                removeViewBox: false, // https://github.com/svg/svgo/issues/1128
+export default defineConfig(({ mode }) => {
+  return {
+    base: "",
+    plugins: [
+      react(),
+      svgr(),
+      ViteImageOptimizer({
+        test: /\.(jpe?g|png|gif|tiff|webp|svg|avif)$/i,
+        exclude: undefined,
+        include: undefined,
+        includePublic: true,
+        logStats: true,
+        ansiColors: true,
+        svg: {
+          multipass: true,
+          plugins: [
+            {
+              name: "preset-default",
+              params: {
+                overrides: {
+                  cleanupNumericValues: true,
+                  removeViewBox: false, // https://github.com/svg/svgo/issues/1128
+                },
+                cleanupIDs: {
+                  minify: true,
+                  remove: false,
+                },
+                convertPathData: false,
               },
-              cleanupIDs: {
-                minify: true,
-                remove: false,
+            },
+            "sortAttrs",
+            {
+              name: "addAttributesToSVGElement",
+              params: {
+                attributes: [{ xmlns: "http://www.w3.org/2000/svg" }],
               },
-              convertPathData: false,
             },
-          },
-          "sortAttrs",
-          {
-            name: "addAttributesToSVGElement",
-            params: {
-              attributes: [{ xmlns: "http://www.w3.org/2000/svg" }],
-            },
-          },
-        ],
+          ],
+        },
+        png: {
+          // https://sharp.pixelplumbing.com/api-output#png
+          quality: 66,
+        },
+        jpeg: {
+          // https://sharp.pixelplumbing.com/api-output#jpeg
+          quality: 66,
+        },
+        jpg: {
+          // https://sharp.pixelplumbing.com/api-output#jpeg
+          quality: 66,
+        },
+        tiff: {
+          // https://sharp.pixelplumbing.com/api-output#tiff
+          quality: 66,
+        },
+        // gif does not support lossless compression
+        // https://sharp.pixelplumbing.com/api-output#gif
+        gif: {},
+        webp: {
+          // https://sharp.pixelplumbing.com/api-output#webp
+          lossless: true,
+        },
+        avif: {
+          // https://sharp.pixelplumbing.com/api-output#avif
+          lossless: true,
+        },
+      }),
+      preload(),
+    ],
+    build: {
+      commonjsOptions: {
+        transformMixedEsModules: false,
       },
-      png: {
-        // https://sharp.pixelplumbing.com/api-output#png
-        quality: 66,
-      },
-      jpeg: {
-        // https://sharp.pixelplumbing.com/api-output#jpeg
-        quality: 66,
-      },
-      jpg: {
-        // https://sharp.pixelplumbing.com/api-output#jpeg
-        quality: 66,
-      },
-      tiff: {
-        // https://sharp.pixelplumbing.com/api-output#tiff
-        quality: 66,
-      },
-      // gif does not support lossless compression
-      // https://sharp.pixelplumbing.com/api-output#gif
-      gif: {},
-      webp: {
-        // https://sharp.pixelplumbing.com/api-output#webp
-        lossless: true,
-      },
-      avif: {
-        // https://sharp.pixelplumbing.com/api-output#avif
-        lossless: true,
-      },
-    }),
-    preload(),
-  ],
-  build: {
-    commonjsOptions: {
-      transformMixedEsModules: false,
+      target: "ES2022",
+      outDir: "./build",
     },
-    target: "ES2022",
-    outDir: "./build",
-  },
-  server: {
-    open: true,
-    port: 3000,
-  },
+    server: {
+      open: true,
+      port: 3000,
+    },
+  };
 });
