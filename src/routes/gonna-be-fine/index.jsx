08b4e3fca169e6849ea38fde30ee65deafb8f3c3
actuallyitsnathaniel <nathanielrbowman@gmail.com>
2024-10-15 20:51:30 -0700

    pre-save testing

--- src/routes/gonna-be-fine/index.jsx
+++ src/routes/gonna-be-fine/index.jsx
@@ -1,11 +1,12 @@
 import Newsletter from "../../components/newsletter";
 
 import stuckAtHome from "../../assets/images/discography/Singles_EPs/stuck-at-home_600x600bb.jpg";
+import PreSaveButton from "../../components/pre-save";
 
 const GonnaBeFine = () => {
   return (
     <div className="flex mt-16 w-screen flex-col justify-between text-center mx-auto my-auto p-5 gap-10 text-white items-center text-3xl">
-      <a
+      {/* <a
         href="https://distrokid.com/hyperfollow/ryland/stuck-at-home"
         rel="noopener noreferrer"
         target="_blank"
@@ -17,7 +18,8 @@ const GonnaBeFine = () => {
           className="object-scale-down p-2"
         />
         <button className="text-xl">pre-save "stuck at home"</button>
-      </a>
+      </a> */}
+      <PreSaveButton />
       <Newsletter />
     </div>
   );
