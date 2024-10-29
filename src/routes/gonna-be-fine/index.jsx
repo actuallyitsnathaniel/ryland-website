9e7cb21995e9e002ec45f456159fa5c9ce2f9dc8
actuallyitsnathaniel <nathanielrbowman@gmail.com>
2024-10-29 15:02:17 -0700

    logic update for final release

--- src/routes/gonna-be-fine/index.jsx
+++ src/routes/gonna-be-fine/index.jsx
@@ -5,6 +5,7 @@ import stuckAtHome from "../../assets/images/discography/Singles_EPs/stuck-at-ho
 import fellThrough from "../../assets/images/discography/Singles_EPs/fell-through-600x600bb.jpg";
 import loveYouAgain from "../../assets/images/discography/Singles_EPs/love-you-again-600x600bb.jpg";
 import iWantEveryoneToKnowYou from "../../assets/images/discography/Singles_EPs/i-want-everyone-to-know-you-600x600bb.jpg";
+import gonnaBeFine from "../../assets/images/discography/Singles_EPs/gonna-be-fine-600.jpg";
 
 const GonnaBeFine = () => {
   const PreSave = () => {
@@ -30,6 +31,11 @@ const GonnaBeFine = () => {
         artwork: iWantEveryoneToKnowYou,
         releaseDate: new Date("2024-11-11"),
       },
+      {
+        title: "gonna-be-fine",
+        artwork: gonnaBeFine,
+        releaseDate: new Date("2024-11-17"),
+      },
     ];
 
     // Find the next release based on today's date
@@ -54,8 +60,22 @@ const GonnaBeFine = () => {
         </a>
       </div>
     ) : (
-      <div className="text-gray-100 pt-2">
-        <p>No upcoming releases available for pre-save.</p>
+      <div className="pt-2">
+        <a
+          href={`https://distrokid.com/hyperfollow/ryland/${release.title}`}
+          rel="noopener noreferrer"
+          target="_blank"
+          className="flex flex-col justify-center items-center outline w-72 h-min p-4 rounded-lg bg-gray-500 bg-opacity-40 hover:bg-opacity-60 transition duration-300"
+        >
+          <img
+            src={release.artwork}
+            alt={`${release.title} artwork`}
+            className="object-scale-down p-2 w-full rounded-md"
+          />
+          <button className="text-xl mt-4">
+            I'm Gonna Be Fine EP - out now
+          </button>
+        </a>
       </div>
     );
   };
