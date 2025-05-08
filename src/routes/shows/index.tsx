a0d095ec49e60ce10d1a64733777a5bfe5fbf2ae
actuallyitsnathaniel <nathanielrbowman@gmail.com>
2025-05-07 17:20:12 -0700

    updated for proper env variable implementation

--- src/routes/shows/index.tsx
+++ src/routes/shows/index.tsx
@@ -4,6 +4,7 @@ import pageTransition from "../../util/transitionPage";
 import BandsintownEvents from "../../components/bandsintown-events";
 
 const Shows = () => {
+  const appId = import.meta.env.VITE_BANDSINTOWN_API_KEY;
   useEffect(() => {
     const script = document.createElement("script");
     script.src = "https://widgetv3.bandsintown.com/main.min.js";
@@ -18,10 +19,7 @@ const Shows = () => {
     <main className="flex flex-col min-h-full h-full md:mt-16 text-center p-3 text-white items-center md:justify-center text-3xl">
       <div className="mt-16 md:my-auto" />
       <motion.div className="flex flex-col gap-5 items-center">
-        <BandsintownEvents
-          artistName="ryland"
-          appId="64da41cfdd8e29a5625280647dc777fd"
-        />
+        <BandsintownEvents artistName="ryland" appId={appId} />
 
         <p className="text-2xl">Wanna be first to the show?</p>
         <iframe
